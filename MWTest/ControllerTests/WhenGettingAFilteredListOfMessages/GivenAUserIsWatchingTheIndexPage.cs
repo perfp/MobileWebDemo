839e99c5c20f5e87cbf73d28ec4b8418471ec36d
Per-Frode Pedersen <pfp@tuck.as>
2011-10-25 19:19:19 +0200

    added test project

---
+++ MWTest/ControllerTests/WhenGettingAFilteredListOfMessages/GivenAUserIsWatchingTheIndexPage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MobileWebDemo.Controllers;
+using MobileWebDemo.Tools;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace MWTest.WhenGettingAFilteredListOfMessages
+{
+  
+    [TestClass]
+    [DeploymentItem("Content\\search.xml", "Content")]
+    public class GivenAUserIsWatchingTheIndexPage
+    {
+        XmlResult result;
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            var controller = new HomeController();
+            result = (XmlResult)controller.GetData("type", "Midlertidig stengt");
+        }
+
+        [TestMethod]       
+        public void ThenWeShouldReturnAnXmlDocument()
+        {
+            Assert.IsInstanceOfType(result.ObjectToSerialize, typeof(XDocument));
+        }
+
+        [TestMethod]
+        public void AndTheOutputShouldContainMessages()
+        {
+            var doc = (XDocument)result.ObjectToSerialize;
+            var messages = doc
+               .Element("searchresult")
+               .Element("result-array")
+               .Element("result")
+               .Element("messages")
+               .Elements("message");
+
+            Assert.AreEqual(9, messages.Count());
+
+        }
+    }
+}
---
+++ MWTest/UnitTests/MessageRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Xml.Linq;
+using MobileWebDemo.Infrastructure;
+using MobileWebDemo.Models;
+
+namespace MWTest.UnitTests
+{
+   
+    [TestClass]
+    [DeploymentItem("Content\\search.xml", "Content")]
+    public class MessageRepositoryTests{
+        
+        [TestMethod]       
+        public void CanFilterXmlBasedOnCounty()
+        {
+            var xdoc = XDocument.Load("Content\\search.xml");
+            var messages = new MessageRepository(xdoc);
+
+            var messagePrototype = new Message();
+            messagePrototype.County = "Akershus";
+
+            var filteredList = messages.GetFilteredList(messagePrototype);
+            Assert.AreEqual(26, filteredList.Count());
+        }
+
+        [TestMethod]
+        public void CanFilterXmlBasedOnMessageType()
+        {
+            var xdoc = XDocument.Load("Content\\search.xml");
+            var messages = new MessageRepository(xdoc);
+
+            var messagePrototype = new Message();
+            messagePrototype.MessageType = Message.MessageTypes.MidlertidigStengt;
+
+            var filteredList = messages.GetFilteredList(messagePrototype);
+            Assert.AreEqual(9, filteredList.Count());
+        }
+
+    }
+}
